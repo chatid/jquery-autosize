36e392ffae36669d1bd2f9a0baf72a734291e608
mponizil <misha.ponizil@gmail.com>
2014-03-06 18:41:26 -0500

    Fix UMD order

--- jquery.autosize.js
+++ jquery.autosize.js
@@ -6,7 +6,7 @@
 */
 
 (function (root, factory) {
-  if (typeof define === 'function' && define.amd) define(['jquery'], 'jquery-autosize', factory);
+  if (typeof define === 'function' && define.amd) define('jquery-autosize', ['jquery'], factory);
   else factory(root.jQuery);
 }(this, function($) {
 
